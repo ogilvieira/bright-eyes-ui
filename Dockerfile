83be65543c73a8878f5f047ade07c4f3cb14da41
Gil Vieira <ogilvieira@gmail.com>
2023-09-13 20:33:31 -0300

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ WORKDIR /app
 COPY ./package.json .
 RUN yarn install
 COPY . .
+ENV API_URL=$API_URL
 RUN yarn build
 CMD [ "yarn", "host:prod" ]
--- server-spa.js
+++ server-spa.js
@@ -1,5 +1,5 @@
 const serverFactory = require('spa-server');
-
+console.info(`Api URL: ${process.env.API_URL}`)
 var server = serverFactory.create({
   path: './dist',
   hostname: '0.0.0.0',
